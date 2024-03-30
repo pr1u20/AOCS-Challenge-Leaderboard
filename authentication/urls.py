376b60dfe2bd4e3b65bba23e558bf8005bae7df4
Pedro Rodriguez <pr1u20@soton.ac.uk>
2024-03-30 17:33:45 +0000

    Update debug mode and add static media URL

--- authentication/urls.py
+++ authentication/urls.py
@@ -1,10 +1,12 @@
 from django.contrib import admin
 from django.urls import path, include
 from . import views
+from django.conf.urls.static import static
+from django.conf import settings
 
 urlpatterns = [
     path('', views.home, name='home'),
     path('signup', views.signup, name='signup'),
     path('signin', views.signin, name='signin'),
     path('signout', views.signout, name='signout'),
-]
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
--- submissions/urls.py
+++ submissions/urls.py
@@ -1,8 +1,10 @@
 from django.contrib import admin
 from django.urls import path, include
 from . import views
+from django.conf.urls.static import static
+from django.conf import settings
 
 urlpatterns = [
     path('submit', views.submit_pickle, name='submit'),
     path('leaderboard', views.leaderboard, name='leaderboard'),
-]
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
